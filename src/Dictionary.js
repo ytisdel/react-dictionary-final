27dfe922f2fc502c31c51e3bd65f7a48024cf7ae
ytisdel <93150405+ytisdel@users.noreply.github.com>
2022-03-18 16:54:54 -0700

    Created a new state called results to update the page. Created new component Results which I am sending the results to be re-rendered everytime the page loads. Created another component called Meaning to display the different definitions, parts of speech, and example of the searched word.

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,19 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState(" ");
+  const [results, setResults] = useState(null);
 
   function handleResponse(response) {
     console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition...`);
 
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     console.log(apiUrl);
@@ -40,6 +42,7 @@ export default function Dictionary() {
           </div>
         </div>
       </form>
+      <Results results={results} />
     </div>
   );
 }
